d32906ef440c5e5dc8f8a76bc765cb6a15680b2a
ufzc3mj8 <lorenzo.lopez2@pearson.com>
2013-07-12 13:28:09 -0400

    Arguments fix

--- 32_CAS_EC_CODE_SYNC_JOB.sql
+++ 32_CAS_EC_CODE_SYNC_JOB.sql
@@ -4,7 +4,6 @@ BEGIN
     job_type => 'PLSQL_BLOCK',
     job_action => 'BEGIN CAS_EC_CODE_SYNC_PROC(); END;',
     start_date => SYSTIMESTAMP,
-    --repeat_interval => 'freq=hourly; INTERVAL=12',
     repear_interval => 'FREQ=HOURLY; INTERVAL=4',
     end_date => NULL,
     enabled => TRUE,
